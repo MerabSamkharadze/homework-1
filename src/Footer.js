77b82dcdc023d44d7746ead23f1d1ac457b7d5fe
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-09-21 11:17:18 +0400

    footer component is done!

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,46 @@
 import React from "react";
+import "./Footer.css";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="Footer">
+      <div className="Footer-content">
+        <nav>
+          <ul className="Footer-nav">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/services">Services</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+            <li>
+              <a href="/blog">Blog</a>
+            </li>
+          </ul>
+        </nav>
+
+        <ul className="Footer-links">
+          <li>
+            <a href="/privacy">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="/terms">Terms of Service</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+
+        <p>
+          &copy; {new Date().getFullYear()} My Website. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 }
