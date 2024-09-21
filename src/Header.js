413e075ed183f0311d62aa4b2eab3e26e3fe51e9
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-09-21 11:09:18 +0400

    Header component is done!

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,19 @@
 import React from "react";
+import "./Header.css";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="Header">
+      <div className="Header-logo">Logo</div>
+      <nav>
+        <ul className="Header-ul">
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Contact</li>
+          <li>Blog</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
