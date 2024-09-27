8a16e733ae443acb63332823ca6c82ee82a85776
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-09-28 00:58:22 +0400

    add link tags on footer

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import React from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Content from "./Components/Content/Content";
+import Contact from "./Components/Contact/Contact";
+import About from "./Components/About/About";
 
 export default function App() {
   return (
     <div className="App">
-      <Header />
-
-      <Footer />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Content />}></Route>
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import "./Footer.css";
 
+import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
     <footer className="Footer">
       <div className="Footer-content">
         <nav>
           <ul className="Footer-nav">
-            <li>Home</li>
-            <li>About</li>
+            <li>
+              <Link className="Link" to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="Link" to="/about">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link className="Link" to="/contact">
+                Contact
+              </Link>
+            </li>
             <li>Services</li>
-            <li>Contact</li>
             <li>Blog</li>
           </ul>
         </nav>
