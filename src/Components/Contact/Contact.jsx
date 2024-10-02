7a4ee5db4bb6bb2b93713faceb0f004ffacca7a2
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-10-02 23:54:10 +0400

    add placeholders on contact page inputs

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -10,15 +10,32 @@ export default function Contact() {
       <form>
         <div>
           <label htmlFor="name">სახელი:</label>
-          <input type="text" id="name" name="name" required />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="შეიყვანეთ თქვენი სახელი"
+            required
+          />
         </div>
         <div>
           <label htmlFor="email">ელ. ფოსტა:</label>
-          <input type="email" id="email" name="email" required />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="შეიყვანეთ თქვენი email"
+            required
+          />
         </div>
         <div>
           <label htmlFor="message">მესიჯი:</label>
-          <textarea id="message" name="message" required></textarea>
+          <textarea
+            id="message"
+            name="message"
+            placeholder="ტექსტი"
+            required
+          ></textarea>
         </div>
         <Button content={"გაგზავნა"} />
       </form>
