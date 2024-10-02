301b295f38b3a76646fb427d72cf72e9cdac4e35
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-10-02 19:12:35 +0400

    add styles on blog component

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,6 +1,12 @@
-import "./Blog.css";
 import React from "react";
+import "./Blog.css";
+import BlogPosts from "../BlogPosts/BlogPosts";
 
 export default function Blog() {
-  return <section className="Blog">Blog</section>;
+  return (
+    <section className="Blog">
+      {" "}
+      <BlogPosts />
+    </section>
+  );
 }
