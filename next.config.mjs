f77edaf1adc9daa376e37ec602fc69dfcdf10fc1
Mebo <mebo@Mebos-MacBook-Air.local>
2024-10-08 19:27:18 +0400

    add loader on posts page. add posts and dynamic rout for each posts

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // Outputs a Single-Page Application (SPA).
-  distDir: "./build", // Changes the build output directory to `./dist`.
-};
+// @ts-check
 
-export default nextConfig;
+export default (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    /* config options here */
+  };
+  return nextConfig;
+};
---
+++ src/app/blog/[id]/page.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import "./page.css";
+
+async function fetchPosts(id) {
+  const response = await axios.get(`https://dummyjson.com/posts/${id}`);
+  return response.data;
+}
+
+export default async function Page({ params }) {
+  const posts = await fetchPosts(params.id);
+
+  return (
+    <div className="post-container">
+      <h1 className="post-title">{posts.title}</h1>
+      <p className="post-body">{posts.body}</p>
+
+      <div className="post-meta">
+        <strong>Tags:</strong> {posts.tags.join(", ")}
+      </div>
+
+      <div className="post-reactions">
+        <strong>👍</strong>
+        {posts.reactions.likes} | <strong>👎</strong> {posts.reactions.dislikes}
+      </div>
+
+      <div className="post-views">
+        <strong>Views:</strong> {posts.views}
+      </div>
+    </div>
+  );
+}
