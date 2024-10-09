d4e5d1ebc031c2f0c5ac7ec1becd6e6a48e9c5c9
MerabSamkharadze <samkharadzemerab@gmail.com>
2024-10-09 21:21:23 +0400

    add reactions on posts

--- src/Components/BlogPosts/BlogPosts.jsx
+++ src/Components/BlogPosts/BlogPosts.jsx
@@ -1,23 +1,13 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import Button from "../Button/Button";
 import Loader from "../Loader/Loader";
 import "./BlogPosts.css";
 import axios from "axios";
 import Link from "next/link";
 
-const BlogPost = ({ title, content }) => {
-  return (
-    <div className="post">
-      <h2 className="post-title">{title}</h2>
-      <p className="post-content">{content}</p>
-    </div>
-  );
-};
-
 export default function BlogPosts() {
   const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true); // State to manage loading
+  const [loading, setLoading] = useState(true);
 
   async function getPosts() {
     try {
@@ -26,7 +16,7 @@ export default function BlogPosts() {
     } catch (error) {
       console.error("Error fetching posts:", error);
     } finally {
-      setLoading(false); // Set loading to false after fetching
+      setLoading(false);
     }
   }
 
@@ -37,13 +27,23 @@ export default function BlogPosts() {
   return (
     <div className="container">
       <h1 className="page-title">Posts</h1>
-      {loading ? ( // Show loader while loading
+      {loading ? (
         <Loader />
       ) : (
         <div className="posts-container">
           {posts.map((post) => (
             <Link key={post.id} href={`/blog/${post.id}`} className="Link">
-              <BlogPost title={post.title} content={post.body} />
+              <div className="post">
+                <h2 className="post-title">{post.title}</h2>
+                <p className="post-content">{post.body}</p>
+                <div className="reactions-container">
+                  <div className="like-dislike">
+                    <div className="like">{post.reactions.likes}</div>
+                    <div className="dislike">{post.reactions.dislikes}</div>
+                  </div>
+                  <div className="views">{post.views}</div>
+                </div>
+              </div>
             </Link>
           ))}
         </div>
