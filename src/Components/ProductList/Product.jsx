abd0520abda1d515cc0e58df876ee30e32d0fb22
MerabSamkharadze <samkharadzemerab@gmail.com>
2024-10-11 12:12:13 +0400

    from csr to ssr : productList components and products page

--- src/Components/ProductList/Product.jsx
+++ src/Components/ProductList/Product.jsx
@@ -1,53 +1,38 @@
-"use client";
-
 import "./ProductList.css";
-import { useEffect, useState } from "react";
-import Loader from "../Loader/Loader";
-import axios from "axios";
 import Product from "../Product/Product";
 import Link from "next/link";
 
-export default function ProductList() {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  async function getProducts() {
-    try {
-      const res = await axios.get("https://dummyjson.com/products");
-      setProducts(res.data.products);
-    } catch (error) {
-      console.error("Error fetching products:", error);
-    } finally {
-      setLoading(false);
-    }
+async function getProducts() {
+  try {
+    const res = await fetch("https://dummyjson.com/products");
+    const data = await res.json();
+    return data.products;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return [];
   }
+}
 
-  useEffect(() => {
-    getProducts();
-  }, []);
-
+export default async function ProductList() {
+  const products = await getProducts();
   return (
     <div className="container">
-      {loading ? (
-        <Loader />
-      ) : (
-        <div className="product-grid">
-          {products.map((product) => (
-            <Link
-              className="Link"
-              href={`/products/${product.id}`}
-              key={product.id}
-            >
-              <Product
-                title={product.title}
-                description={product.description}
-                image={product.thumbnail}
-                price={product.price}
-              />
-            </Link>
-          ))}
-        </div>
-      )}
+      <div className="product-grid">
+        {products.map((product) => (
+          <Link
+            className="Link"
+            href={`/products/${product.id}`}
+            key={product.id}
+          >
+            <Product
+              title={product.title}
+              description={product.description}
+              image={product.thumbnail}
+              price={product.price}
+            />
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
