6d8950149d3e74e27e9ce52c2968673914f15d58
MerabSamkharadze <samkharadzemerab@gmail.com>
2024-10-11 13:14:11 +0400

    add welcome text and registration button on home page

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -2,12 +2,15 @@
 import React from "react";
 import "./Button.css";
 
-export default function Button({ content }) {
+export default function Button({ content, backgroundColor }) {
   function prevent(e) {
     e.preventDefault();
   }
+  const style = {
+    backgroundColor,
+  };
   return (
-    <button onClick={prevent} className="custom-button">
+    <button onClick={prevent} style={style} className={`custom-button`}>
       {content}
     </button>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,14 @@
 import "./Page.css";
-
+import Button from "@/Components/Button/Button";
 export default function Content() {
-  return <div className="main-container"></div>;
+  return (
+    <div className="main-container">
+      <h1 className="hello-text">welcome to Geo Market</h1>
+      <h2 className="losung-text">
+        Here you will find all the products you are looking for.
+      </h2>
+
+      <Button content={"Log In / Registration"} backgroundColor={"brown"} />
+    </div>
+  );
 }
