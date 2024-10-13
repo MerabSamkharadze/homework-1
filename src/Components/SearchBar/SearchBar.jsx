5d953c47d37c804d68c27ee0db405eb32a2a4c44
MerabSamkharadze <samkharadzemerab@gmail.com>
2024-10-13 20:36:06 +0400

    fixed bug: debounce function works properly now! (send only one request to server)

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,35 +1,33 @@
-// SearchBar.js
 "use client";
 
 import "./SearchBar.css";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 
-// Debounce function
-function debounce(func, delay) {
-  let timeoutId;
-  return (...args) => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      func(...args);
-    }, delay);
-  };
-}
-
 export default function SearchBar() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [debouncedSearchQuery, setDebouncedSearchQuery] = useState(searchQuery);
   const router = useRouter();
 
-  const handleSearch = debounce((value) => {
-    router.push(`/products?search=${value}`);
-  }, 2000);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedSearchQuery(searchQuery);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  useEffect(() => {
+    if (debouncedSearchQuery) {
+      router.push(`/products?search=${debouncedSearchQuery}`);
+    }
+  }, [debouncedSearchQuery, router]);
 
   const handleSearchChange = (e) => {
     const value = e.target.value;
     setSearchQuery(value);
-    handleSearch(value);
   };
 
   return (
