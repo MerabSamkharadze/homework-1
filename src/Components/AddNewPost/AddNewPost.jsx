cd7d03583e1111f71b0db9e84d1a76312fc03e8a
MerabSamkharadze <samkharadzemerab@gmail.com>
2024-10-19 20:34:06 +0400

    create addNewPost component and add it on blog's page

---
+++ src/Components/AddNewPost/AddNewPost.jsx
@@ -0,0 +1,10 @@
+import "./AddNewPost.css";
+import AddSvg from "@/public/svg/AddSvg";
+
+export default function AddNewPost() {
+  return (
+    <div className="addIcon">
+      <AddSvg />
+    </div>
+  );
+}
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Blog.css";
 import BlogPosts from "../../Components/BlogPosts/BlogPosts";
+import AddNewPost from "@/Components/AddNewPost/AddNewPost";
 
 export default function Blog() {
   return (
     <section className="Blog">
-      {" "}
+      <AddNewPost />
       <BlogPosts />
     </section>
   );
