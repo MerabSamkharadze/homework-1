63bc47026c365528de6bb5d917a68936ba9868ad
Kadi033 <demetre33dev@gmail.com>
2024-10-24 20:32:47 +0400

    შევცვალე login ის გვერდი ასევე profile , blog და addBlog ის სტილი

--- src/app/(authorized)/profile/page.jsx
+++ src/app/(authorized)/profile/page.jsx
@@ -1,7 +1,6 @@
 import { cookies } from "next/headers";
 import { refreshAccessToken } from "../../../lib/action";
-import "./profile.css";
-
+import "./Profile.css";
 export default async function Profile() {
   const cookieStore = cookies();
   let token = cookieStore.get("accessToken");
@@ -29,52 +28,51 @@ export default async function Profile() {
   }
 
   return (
-    <div className="profile-container">
-      {data ? (
-        <div className="profile-card">
-          <div className="profile-header">
+    <section className="profile-section">
+      <div className="profile-container">
+        <h1 className="profile-title">Profile</h1>
+        <p className="profile-intro">I'm a creative PHP web developer</p>
+        <div className="profile-layout">
+          <div className="profile-about">
+            <h4>About me</h4>
+            <p>
+              I am an all-round web developer. I am a senior programmer with
+              good knowledge of front-end techniques. Vitae sapien pellentesque
+              habitant morbi tristique senectus et. Aenean sed adipiscing diam
+              donec adipiscing tristique risus.
+            </p>
+          </div>
+          <div className="profile-image">
             <img
               src={data.image}
               alt={`${data.username} ${data.lastName}`}
-              className="profile-image"
+              className="profile-pic"
             />
-            <h1 className="profile-name">{data.username}</h1>
           </div>
-          <div className="profile-info">
-            <p>
-              <strong>Email:</strong> {data.email}
-            </p>
-            <p>
-              <strong>Phone:</strong> {data.phone}
-            </p>
-            <p>
-              <strong>Age:</strong> {data.age}
-            </p>
-            <p>
-              <strong>Height:</strong> {data.height} cm
-            </p>
-            <p>
-              <strong>Weight:</strong> {data.weight} kg
-            </p>
-            <p>
-              <strong>Blood Group:</strong> {data.bloodGroup}
-            </p>
-            <p>
-              <strong>Eye Color:</strong> {data.eyeColor}
-            </p>
-            <p>
-              <strong>Hair:</strong> {data.hair.color} ({data.hair.type})
-            </p>
-            <p>
-              <strong>Address:</strong> {data.address.address},{" "}
-              {data.address.city}, {data.address.state},{" "}
-              {data.address.postalCode}, {data.address.country}
-            </p>
+          <div className="profile-details">
+            <h4>Details</h4>
+            <div>
+              <span className="bold-text">
+              Name:
+                <p>
+                   {data.username} {data.lastName}
+                </p>{" "}
+              </span>
+              <span className="bold-text">
+              Age:
+                <p> {data.age} years</p>{" "}
+              </span>
+              <span className="bold-text">
+              Location:
+                <p>
+                   {data.address.city}, {data.address.state},{" "}
+                </p>
+              </span>
+            </div>
+
           </div>
         </div>
-      ) : (
-        <p>No user data found.</p>
-      )}
-    </div>
+      </div>
+    </section>
   );
 }
