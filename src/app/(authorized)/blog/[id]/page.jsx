12d0464b15afc919713d7ede4b267e733457792d
MerabSamkharadze <samkharadzemerab@gmail.com>
2024-11-03 20:01:55 +0400

    migration to tailwind : posts dynamic route

--- src/app/(authorized)/blog/[id]/page.jsx
+++ src/app/(authorized)/blog/[id]/page.jsx
@@ -19,23 +19,28 @@ export default async function Page({ params }) {
   }
 
   return (
-    <div className="post-container">
-      <div className="kadi-container">
-      <h1 className="post-title">{post.title}</h1>
-      <p className="post-body">{post.body}</p>
-
-      <div className="post-meta">
-        <strong>Tags:</strong> {post.tags.join(", ")}
+    <div
+      className="flex justify-center items-center h-screen bg-black bg-cover text-white text-start p-5 mt-12"
+      style={{ backgroundImage: "url('/assets/1280417267661803541.webp')" }}
+    >
+      <div className="bg-black bg-opacity-30 backdrop-blur-md max-w-3xl mx-auto p-10 rounded-lg border border-white">
+        <h1 className="text-white text-5xl md:text-3xl mb-8 text-start shadow-md">
+          {post.title}
+        </h1>
+        <p className="text-gray-400 text-xl md:text-base leading-relaxed mb-8 p-2.5 rounded">
+          {post.body}
+        </p>
+        <div className="italic text-lg mt-6 text-white">
+          <strong>Tags:</strong> {post.tags.join(", ")}
+        </div>
+        <div className="flex justify-around w-full p-6 rounded text-lg text-white">
+          <div className="like-dislikes">👍 {post.reactions.likes}</div>
+          <div className="like-dislikes">👎 {post.reactions.dislikes}</div>
+        </div>
+        <div className="text-center mt-10 text-white text-2xl md:text-xl">
+          Views: {post.views}
+        </div>
       </div>
-
-      <div className="post-reactions">
-        <div className="like-dislikes">👍 {post.reactions.likes}</div>
-        <div className="like-dislikes">👎 {post.reactions.dislikes}</div>
-      </div>
-
-      <div className="post-views">Views: {post.views}</div>
-      </div>
-      
     </div>
   );
 }
