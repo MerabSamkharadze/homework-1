241f55af8c42ef86960b65af871ddd878d0fbb2a
MerabSamkharadze <samkharadzemerab@gmail.com>
2024-11-07 19:45:21 +0400

    migration to tailwind : updateproduct component

--- src/Components/UpdateProductForm/UpdateProductForm.jsx
+++ src/Components/UpdateProductForm/UpdateProductForm.jsx
@@ -19,15 +19,18 @@ export default function UpdateProductForm({ product, onSubmit }) {
   };
 
   return (
-    <form className="product-container55" onSubmit={handleSubmit}>
-      <h2>Edit Product</h2>
+    <form
+      className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col bg-opacity-35 bg-gray-800 backdrop-blur-lg p-5 rounded-lg shadow-lg w-[60rem]"
+      onSubmit={handleSubmit}
+    >
+      <h2 className="mb-5 text-2xl text-white text-center">Edit Product</h2>
       <input
         type="text"
         name="title"
         placeholder="Product Title"
         value={updatedProduct.title}
         onChange={handleChange}
-        className="form-input"
+        className="mb-4 p-2 text-base text-black border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none"
       />
       <input
         type="text"
@@ -35,7 +38,7 @@ export default function UpdateProductForm({ product, onSubmit }) {
         placeholder="Product Description"
         value={updatedProduct.description}
         onChange={handleChange}
-        className="form-input"
+        className="mb-4 p-2 text-base border text-black border-gray-300 rounded-md focus:border-blue-500 focus:outline-none"
       />
       <input
         type="number"
@@ -43,9 +46,12 @@ export default function UpdateProductForm({ product, onSubmit }) {
         placeholder="Product Price"
         value={updatedProduct.price}
         onChange={handleChange}
-        className="form-input"
+        className="mb-4 p-2 text-base border text-black border-gray-300 rounded-md focus:border-blue-500 focus:outline-none"
       />
-      <button type="submit" className="update-button">
+      <button
+        type="submit"
+        className="px-4 py-2 bg-blue-500 text-white text-base rounded-md hover:bg-blue-700"
+      >
         Update Product
       </button>
     </form>
